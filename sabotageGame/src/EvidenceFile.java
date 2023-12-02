02cc0b687effb009cf2143a1236d9569654b7a33
jbrady3 <jbrady3@uiowa.edu>
2023-12-01 19:41:07 -0600

    EvidenceFile added addEvidence method or the main method of EvidenceFile

--- sabotageGame/src/EvidenceFile.java
+++ sabotageGame/src/EvidenceFile.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class EvidenceFile {
     /**
      * Evidence class that holds the positive and negative values
@@ -31,4 +32,55 @@ public class EvidenceFile {
             this.inspector = inspector;
         }
     }
+
+    private ArrayList<Evidence> mainFile;
+    private Player suspect;
+    private int good;
+    private int bad;
+
+    public Player getSuspect(){
+        return this.suspect;
+    }
+
+    private int getGood(){
+        return this.good;
+    }
+    public int getBad() {
+        return this.bad;
+    }
+    public void addPoints(boolean morality){
+        if(morality){
+            this.good = this.good+1;
+        }else{
+            this.bad = this.bad+1;
+        }
+    }
+    private ArrayList<Evidence> grabFile(){
+        return this.mainFile;
+    }
+
+    /**
+     * addEvidence takes in an EvidenceFile to add into itself by navigating through itself to ensure that the evidence being
+     * added doesn't come from the same source with the same information and also increases the values of good or bad
+     * @param file
+     */
+    public void addEvidence(EvidenceFile file){
+        boolean preExists = true;
+        for(int x = 0;x< file.grabFile().size();x++){
+
+            for(int y = 0;y<mainFile.size();y++){
+                if(file.grabFile().get(x).getInspector().equals(mainFile.get(y).getInspector()) && file.grabFile().get(x).isDoubleAgent() == mainFile.get(y).isDoubleAgent()){
+                    preExists = false;
+                }
+            }
+            if(preExists){
+                mainFile.add(file.grabFile().get(x));
+                addPoints(file.grabFile().get(x).isDoubleAgent());
+            }
+
+        }
+
+    }
+
+
 }
--- sabotageGame/src/GameState.java
+++ sabotageGame/src/GameState.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
  * GameState is going to be the class that holds the details of play state
  */
 public class GameState {
-    private ArrayList<Player>
+    private ArrayList<Player> rock;
 
 
 
