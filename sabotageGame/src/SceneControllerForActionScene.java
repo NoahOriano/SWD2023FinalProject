388474e23f7401778c02d805ee7bff7c17069d53
noriano <noriano@uiowa.edu>
2023-12-02 14:04:23 -0600

    SceneChanging hierarchy

--- sabotageGame/src/SceneControllerForActionScene.java
+++ sabotageGame/src/SceneControllerForActionScene.java
@@ -24,7 +24,7 @@ import java.net.Socket;
  * Contains all different window elements and handling for all three scenes
  * This is done to allow sharing of elements, such as chat, between scenes
  */
-public class SceneControllerForActionScene {
+public class SceneControllerForActionScene extends SceneController{
     /**
      * Text area for the chat log
      */
@@ -50,87 +50,20 @@ public class SceneControllerForActionScene {
     Button textSubmit;
 
     /**
-     * Host server button for game start scene
-     */
-    @FXML
-    Button hostServerButton;
-
-    /**
-     * Join server button for game start scene
-     */
-    @FXML
-    Button joinServerButton;
-
-    /**
-     * Input field for server port when joining a server
+     * Input field for displaying port when on a server
      */
     @FXML
     TextField serverPortField;
 
     /**
-     * Input field for server IP when joining a server
+     * Input field for displaying IP when on a server
      */
     @FXML
     TextField serverIPField;
 
-    /**
-     * Connection to the server
-     */
-    private Socket connection;
-    /**
-     * Output stream for messages over the network
-     */
-    private ObjectOutputStream output;
-    /**
-     * Input stream for messages over the network
-     */
-    private ObjectInputStream input;
     @FXML
     public void initialize(){
-        if(joinServerButton != null) {
-            joinServerButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    try {
-                        connection = new Socket(serverIPField.getText(), Integer.parseInt(serverPortField.getText()));
-                        output = new ObjectOutputStream(connection.getOutputStream());
-                        input = new ObjectInputStream(connection.getInputStream());
-                        output.writeObject(new NetworkMessage(MessageValues.MESSAGE, "Client Connected", null));
-                        output.flush();
-                        try {
-                            setSignOnScreen(actionEvent);
-                        } catch (IOException e) {
-                            System.out.println("Connected, but failed to switch screens");
-                        }
-                    } catch (IOException e) {
-                        System.out.println("NOT CONNECTED");
-                    }
-                }
-            });
-        }
-    }
 
-    public void setGameJoinScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("GameJoinScene.fxml"));
-        Scene scene = new Scene(root);      // attach scene graph to scene
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);              // attach scene to stage
-        stage.show();                       // display the stage
-    }
-    public void setGameActionScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ActionScene.fxml"));
-        Scene scene = new Scene(root);      // attach scene graph to scene
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);              // attach scene to stage
-        stage.show();                       // display the stage
-    }
-    public void setSignOnScreen(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("SignOnScreen.fxml"));
-        loader.setController(this);
-        Scene scene = new Scene(loader.load());      // attach scene graph to scene
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);              // attach scene to stage
-        stage.show();                       // display the stage
     }
 
     // Need to include a way to select and do actions, but that is more complicated
