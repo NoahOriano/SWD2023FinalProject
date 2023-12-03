1af3b5da4038139915a110dd5f9cbcdbc66360c9
jbrady3 <jbrady3@uiowa.edu>
2023-12-03 12:35:04 -0600

    New logic for EvidenceList

--- sabotageGame/src/EvidenceList.java
+++ sabotageGame/src/EvidenceList.java
@@ -4,29 +4,50 @@ import java.util.ArrayList;
 
 public class EvidenceList {
     private ArrayList<Evidence> evidenceList;
-    private int CultistCount;
-    private int InnocentCount;
 
-    /**
+    private int cultistCount;
+
+    private int innocentCount;
+
+    private String prosecutor;
+    private String defense;
+
+    /**Client End Method
      * Add the evidence given to the file, used for player end
      * @param identifier
      * @return
      */
     public boolean addEvidence(PlayerIdentifier identifier){
+        evidenceList.add(new Evidence(defense, identifier));
+        return true;
+        }
 
-    }
-
-    /**
+    /**Server End Method
      * Add the evidence given to the file, used for server end since it must store inspector
      * @param identifier
      * @return
      */
     public boolean addEvidence(PlayerIdentifier identifier, String inspector){
-
+        boolean added = true;
+for(int x = 0; x< evidenceList.size();x++){
+    if(evidenceList.get(x).getInvestigator().equals(inspector)&&evidenceList.get(x).getIdentifier().equals(identifier)){
+        added = false;
+    }
+}
+if(added){
+    evidenceList.add(new Evidence(this.defense,identifier,this.prosecutor));
+}
+return added;
     }
 
-    public Evidence getEvidenceByInsvestigatorName(String name){
-
+    public Evidence getEvidenceByInvestigatorName(String name){
+        Evidence evidenceByName = new Evidence(null, null);
+for(int x = 0; x<evidenceList.size();x++){
+    if(evidenceList.get(x).getInvestigator().equals(name)){
+        evidenceByName = evidenceList.get(x);
+    }
+}
+return evidenceByName;
     }
 
     public getEvidenceByIndex(){
