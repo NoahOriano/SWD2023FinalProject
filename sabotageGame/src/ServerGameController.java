5ef830c9b313c3832a52120ebcbb04dceacf923a
jbrady3 <jbrady3@uiowa.edu>
2023-12-03 16:19:26 -0600

    Steal method should be completed

--- sabotageGame/src/ServerGameController.java
+++ sabotageGame/src/ServerGameController.java
@@ -16,6 +16,7 @@ public class ServerGameController {
         }
         return index;
     }
+
     public int getListIndex(String playerID, int playerStatesIndex){
         int listIndex = 0;
         for(int x=0;x<playerStates.get(playerStatesIndex).getPlayerFiles().size();x++ ){
@@ -25,34 +26,43 @@ public class ServerGameController {
         }
         return listIndex;
     }
-    /*
-    public boolean addRandomEvidence()
-
-     */
 
 
-    public void steal(String thief, String victim){
+    public void steal(String thief, String victim) {
         int locationThief = getIndex(thief);
         int locationVictim = getIndex(victim);
+        boolean added = false;
+        int counter = 0;
 
         SecureRandom gen = new SecureRandom();
 
-        int randomNumber = gen.nextInt(playerStates.get(locationVictim).getPlayerFiles().size());
-        int secondNUmber = gen.nextInt(playerStates.get(locationVictim).getPlayerFiles().get(randomNumber).getEvidenceList().size());
-
-        int thiefEvidenceListIndex = getListIndex(victim,locationVictim);
-
-        boolean checker = false;
-        int counter = 0;
+        while (!added) {
+            int randomNumber = gen.nextInt(playerStates.get(locationVictim).getPlayerFiles().size()); //Index of the random evidenceFile taken from victim
+            int evidenceSize = playerStates.get(locationVictim).getPlayerFiles().get(randomNumber).getEvidenceList().size();
 
 
-        //EvidenceList searched = getEvidenceList(victim)
+            int secondNumber = gen.nextInt(evidenceSize); //Index of the random evidence from the evidence file taken from victim
 
+            String matcher = playerStates.get(locationVictim).getPlayerFiles().get(randomNumber).getDefense();
+            int thiefListIndex = getListIndex(matcher, locationThief); //Index of the same random evidenceFile being grabbed from victim
+            EvidenceList thiefEvidence = playerStates.get(locationThief).getPlayerFiles().get(thiefListIndex);
+
+            Evidence randomEvidence = playerStates.get(locationVictim).getPlayerFiles().get(randomNumber).getEvidenceList().get(secondNumber);
+
+            boolean check = thiefEvidence.addEvidence(randomEvidence.getIdentifier(), randomEvidence.getInvestigator());
+            if (check) {
+                counter++;
+                if (counter == 3) {
+                    added = true;
+                }
+            }
         }
-
-
+    }
 
     public void pass(String user, String infoAbout, String recipient){
+int userIndex = getIndex(user);
+int recipientIndex = getIndex(recipient);
+
 
     }
 
