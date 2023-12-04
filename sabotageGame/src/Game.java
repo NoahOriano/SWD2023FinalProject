6eb2cc73a540ddbbcdf32baebcde81b2537353ed
egladden <elijah-gladden@uiowa.edu>
2023-12-04 04:24:54 -0600

    fixed text box on scene builder.

--- sabotageGame/src/Game.java
+++ sabotageGame/src/Game.java
@@ -5,19 +5,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * 
+ * A class called game that is used as the main driver for play. The driver must be run each time a player
+ * wishes to start playing and join a game. Once ran, it will first prompt the user to enter the IP address
+ * and port number of the game they either wish to join or host.
  */
-public class Game extends Application {
+public class Game extends Application {// A public class called Game that extends application.
     public static void main(String[] args) {
         launch(args);
-    }
+    }// Launch the game.
+
+    /** The overridden start() method of the Game class that overrides the generic start() method of java.**/
     @Override
     public void start(Stage stage) throws Exception {
         Parent root;
         root = FXMLLoader.load(getClass().getResource("GameJoinScene.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Sabotage");
+        stage.setTitle("Dawn of Doom");// Set the title of the game to the game name.
         stage.setScene(scene);
-        stage.show();                       // display the stage
+        stage.show();// display the stage
     }
 }
