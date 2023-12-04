bf012c519d1d495a08a40e6dec026ad5713fdaad
egladden <elijah-gladden@uiowa.edu>
2023-12-04 05:03:29 -0600

    PlayerIdentifier comments done.

--- dawnOfDoom/src/PlayerIdentifier.java
+++ dawnOfDoom/src/PlayerIdentifier.java
@@ -2,6 +2,6 @@
  * PlayerIdentifier is the class used to dicatate the role of the players, and is not simply just a boolean to allow
  * further expansions on the game with different roles
  */
-public enum PlayerIdentifier {
+public enum PlayerIdentifier {// A player can either be a cultist or an innocent.
     CULTIST, INNOCENT
 }
