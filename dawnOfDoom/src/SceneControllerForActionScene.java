1cb55b387d0fa35d0915059182683ad136b59509
jbrady3 <jbrady3@uiowa.edu>
2023-12-04 04:40:09 -0600

    Pass Implemented

--- dawnOfDoom/src/SceneControllerForActionScene.java
+++ dawnOfDoom/src/SceneControllerForActionScene.java
@@ -147,8 +147,7 @@ public class SceneControllerForActionScene extends SceneController {
                         sendMessage(new NetworkMessage(MessageValue.STEAL, playerOptions.getValue(), null, null));
                     } else if (actionOptions.getValue().equals("Pass")) {
                         sendMessage(new NetworkMessage(MessageValue.PASS, playerOptions.getValue(), null, null));
-                    }
-                    else if (actionOptions.getValue().equals("Forge")) {
+                    } else if (actionOptions.getValue().equals("Forge")) {
                         sendMessage(new NetworkMessage(MessageValue.FORGE, playerOptions.getValue(), null, null));
                     } else if (actionOptions.getValue().equals("Investigate")) {
                         sendMessage(new NetworkMessage(MessageValue.INVESTIGATE, playerOptions.getValue(), null, null));
